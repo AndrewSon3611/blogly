025b53bb45b352cbbd53e5fbbc2de490fae1120d
AndrewSon3611 <andrewson3611@gmail.com>
2023-06-18 02:19:45 -0400

    part 2 added posts

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 """Blogly application."""
 
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, flash
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
@@ -19,6 +19,10 @@ db.create_all()
 @app.route('/')
 def root():
     return redirect("/users")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 @app.route('/users')
 def users_index():
@@ -89,3 +93,49 @@ def users_delete(user_id):
     db.session.commit()
 
     return redirect("/users")
+
+
+@app.route('/users/<int:user_id>/posts/new')
+def new_post_form(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('posts/new.html', user=user)
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def new_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_p = Post(title=request.form['title'], content=request.form['content'], user=user)
+
+    db.session.add(new_p)
+    db.session.commit()
+    flash(f"Post'{new_p.title}' added!")
+    return redirect(f"/users/{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def show_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('posts/show.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('/posts/edit.html', post=post)
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def update_posts(post_id):
+    post=Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' edited!")
+    return redirect(f"/users/{post.user_id}")
+
+@app.route('/posts/<int:post_id>/delete', methods={"POST"})
+def delete_posts(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    flash(f"Post '{post.title}' has been deleted!")
+
+    return redirect(f"/users/{post.user_id}")
